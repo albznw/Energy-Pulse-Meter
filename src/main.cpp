136fa4e9518ba5f7e88f0afeaef3b69ebb5777f4
Albin Winkelmann <albin-w@hotmail.com>
2019-02-07 23:16:32 +0100

    First version of code done

--- src/main.cpp
+++ src/main.cpp
@@ -11,22 +11,29 @@
 #include <TaskScheduler.h>
 #include <ESP8266WiFi.h>
 #include <EEPROM.h>
+#include <NTPClient.h>
+#include <ArduinoJson.h>
+#include <TimeLib.h>
 
 #include <debugHeaders.h>
-
 
 #include "secrets.h" // is needed for wifi, ota and mqtt
 #include "eepromReadWrite.hpp"
 
 /******************************** Variables ***********************************/
+int currentDay, lastDay; // keep track of which day it is
+
 //Number of pulses, used to measure energy.
-unsigned long pulseCount = 0;
+unsigned long pulseCount = 0; // resets every day
 
 //Used to measure power.
-unsigned long pulseTime, lastPulseTime;
+unsigned long lastPulseTime;
 
 //power and energy
-double power, elapsedkWh;
+double power = 0;
+double todaykWh = 0;
+double yesterdaykWh = 0;
+double totalkWh = 0;
 
 //Number of pulses per wh - found or set on the meter.
 int ppwh = 1; //1000 pulses/kwh = 1 pulse per wh
@@ -34,6 +41,8 @@ int ppwh = 1; //1000 pulses/kwh = 1 pulse per wh
 // Used for status leds
 unsigned long onMs, offMs; 
 int colors, blinkTimes;
+
+#define SENSOR_COOLDOWN 20 // ms
 
 /**************************** General - Settings ******************************/
 #define OTA_HOSTNAME                "EnergyPulseMeter" // Leave empty for esp8266-[ChipID]
@@ -50,15 +59,16 @@ int colors, blinkTimes;
 bool OTA_ON = true; // Turn on OTA
 
 /****************************** MQTT - Settings *******************************/
-// Connection things is found in Secret.h
-#define MQTTClientId        "logitech_z906"
-#define MQTTCategory        "speaker"
+// Connection things is found in secret.h
+#define MQTTClientId        "energy_pulse_meter"
+#define MQTTCategory        "sensor"
 
 #define ClientRoot          MQTTCategory "/" MQTTClientId
 
 // Some examples on how the routes should be
-#define CommandTopic        ClientRoot "/cmnd/json"
+#define CommandTopic        ClientRoot "/cmnd/"
 #define StateTopic          ClientRoot "/state/json"
+#define TelemetryTopic      ClientRoot "/tele"
 #define DebugTopic          ClientRoot "/debug"
 #define WillTopic           ClientRoot "/will"
 #define WillQoS             0
@@ -71,6 +81,14 @@ WiFiClient wificlient;  // is needed for the mqtt client
 PubSubClient mqttclient;
 
 /*********************************** Tasks ************************************/
+WiFiUDP ntpUDP;
+
+// You can specify the time server pool and the offset (in seconds, can be
+// changed later with setTimeOffset() ). Additionaly you can specify the
+// update interval (in milliseconds, can be changed using setUpdateInterval() ).
+NTPClient timeClient(ntpUDP, "se.pool.ntp.org", 3600, 60000);
+
+/*********************************** Tasks ************************************/
 Scheduler taskManager;
 #define HALF_MINUTE 30000UL
 
@@ -80,16 +98,22 @@ void checkWifiStatus();
 void blinkStatusLed();
 void disableBlinkStatusLed();
 void sensorOneLed();
+void updateTime();
+void sendTelemetry();
 
 Task tSaveToEEPROM(TASK_MINUTE, TASK_FOREVER, &saveToEEPROM, &taskManager);
 Task tCheckWifiStatus(TASK_MINUTE, TASK_FOREVER, &checkWifiStatus, &taskManager);
 Task tBlink(200, 3, &blinkStatusLed, &taskManager, false, NULL, &disableBlinkStatusLed);
 Task tSensorOneLed(150, TASK_FOREVER, &sensorOneLed, &taskManager);
+Task tUpdateTime(TASK_MINUTE, TASK_FOREVER, &updateTime, &taskManager);
+Task tSendTelemetry(HALF_MINUTE, TASK_FOREVER, &sendTelemetry, &taskManager);
 
 /***************************** EEPROM ADDRESSES *******************************/
 // Only writing 32 bit information (4 bytes)
-#define ELAPSED_KWH_ADDR  1
-#define PULSE_COUNT_ADDR  5
+#define PULSE_COUNT_ADDR    1
+#define ELAPSED_KWH_ADDR    5
+#define YESTERDAY_KWH_ADDR  9
+#define TOTAL_KWH_ADDR      13
 
 /*********************************** MISC *************************************/
 #define BLUE    0b001
@@ -184,23 +208,30 @@ void setupWifiManager() {
 }
 
 void saveToEEPROM() {
-  Log("Saving to EEPROM\n");
-  EEPROMWritelong(ELAPSED_KWH_ADDR, elapsedkWh);
-  EEPROMWritelong(PULSE_COUNT_ADDR, pulseCount);
+  Debugf("Saving to EEPROM\n");
+  EEPROMWritelong( (long) PULSE_COUNT_ADDR, pulseCount);
+  EEPROMWritelong( (long) ELAPSED_KWH_ADDR, todaykWh);
+  EEPROMWritelong( (long) YESTERDAY_KWH_ADDR, yesterdaykWh);
+  EEPROMWritelong( (long) TOTAL_KWH_ADDR, totalkWh);
 }
 
 void resetEEPROM() {
-  EEPROMWritelong(ELAPSED_KWH_ADDR, 0);
   EEPROMWritelong(PULSE_COUNT_ADDR, 0);
+  EEPROMWritelong(ELAPSED_KWH_ADDR, 0);
+  EEPROMWritelong(YESTERDAY_KWH_ADDR, 0);
+  EEPROMWritelong(TOTAL_KWH_ADDR, 0);
 }
 
 void readFromEEPROM() {
-  elapsedkWh = (double) EEPROMReadlong(ELAPSED_KWH_ADDR);
   pulseCount = (unsigned long) EEPROMReadlong(PULSE_COUNT_ADDR);
+  todaykWh = (double) EEPROMReadlong(ELAPSED_KWH_ADDR);
+  yesterdaykWh = (double) EEPROMReadlong(YESTERDAY_KWH_ADDR);
+  totalkWh = (double) EEPROMReadlong(TOTAL_KWH_ADDR);
 }
 
-void reconnectWifiCallback() {
-
+void updateTime() {
+  Debugf("Updating time\n");
+  timeClient.update();
 }
 
 void disableBlinkStatusLed() {
@@ -262,14 +293,91 @@ void setStatusLed(uint8_t _colors, long _onMs = 300, long _offMs = 200, int _bli
   tBlink.enable();
 }
 
+void reconnectWifiCallback() {
+
+}
+
 void checkWifiStatus() {
   if(!WiFi.isConnected()) {
     tCheckWifiStatus.setCallback(&reconnectWifiCallback);
   }
 }
 
+bool publishMQTT(const char* topic, const char* payload){
+  String printString = "";
+  bool returnBool = false;
+  if(mqttclient.publish(topic, payload)) {
+    returnBool = true;
+    printString = String("[publishMQTT] '" + String(payload) + "' was sent sucessfully to: ");
+  } else {
+    returnBool = false;
+    printString = String("[publishMQTT] ERROR (" + String(mqttclient.state()) + ") sending: '" + String(payload) + "' to: ");
+  }
+  printString += topic;
+  Traceln(printString);
+  return returnBool;
+}
+
+bool publishMQTT(const char* topic, String payload){
+  return publishMQTT(topic, payload.c_str());
+}
+
+String payloadToString(byte* payload, int length) {
+  char message_buff[length];
+  int i = 0;
+  for (i = 0; i < length; i++) {
+      message_buff[i] = payload[i];
+    }
+  message_buff[i] = '\0';
+  return String(message_buff);
+}
+
+void mqttCallback(char* topic, byte* payload, int length) {
+
+  //convert topic to string to make it easier to work with
+  String topicStr = topic;
+  String payloadStr = payloadToString(payload, length);
+
+  Traceln("[MQTT][callback] Callback update.");
+  Traceln(String("[MQTT][callback] Topic: " + topicStr));
+
+  if(topicStr.equals(CommandTopic)){
+    if(payloadStr.equals("restart")) {
+      ESP.restart();
+    } else if(payloadStr.equals("you on?")) {
+      publishMQTT(StateTopic, "Yes");
+    } else if(payloadStr.equals("reset")) {
+      resetEEPROM();
+      readFromEEPROM();
+    }
+  }
+}
+
+/** Connects to the MQTT broker and subscribes to the topic */
+bool connectMQTT() {
+  Trace("[MQTT] Connecting to MQTT server...");
+  while (!mqttclient.connected()) {
+    //if connected, subscribe to the topic(s) we want to be notified about
+    if(mqttclient.connect(MQTTClientId, MQTTUsername, MQTTPassword)) {
+      Traceln("MTQQ Connected!");
+      if (mqttclient.subscribe(CommandTopic))
+        Trace("[MQTT] Sucessfully subscribed to %s\n", CommandTopic);
+      publishMQTT(DebugTopic, FirstMessage);
+      return true;
+    }
+    Trace("Failed to connect to MQTT Server, state: %d\n", mqttclient.state());
+    return false;
+  } 
+}
+
+/** Be sure to setup WIFI before running this method! */
+void setupMQTT() {
+  mqttclient = PubSubClient(Broker, Port, mqttCallback, wificlient);
+  connectMQTT();
+}
+
 void turnOffSensorOneLed() {
-  TraceFunc("Sensor one indicator led - OFF\n");
+  // TraceFunc("Sensor one indicator led - OFF\n");
   digitalWrite(SENSOR_ONE_INDICATOR_PIN, LOW);
   tSensorOneLed.setCallback(sensorOneLed);
   tSensorOneLed.disable();
@@ -277,42 +385,66 @@ void turnOffSensorOneLed() {
 
 void sensorOneLed() {
   digitalWrite(SENSOR_ONE_INDICATOR_PIN, HIGH);
-  TraceFunc("Sensor one indicator led - ON\n");
+  // TraceFunc("Sensor one indicator led - ON\n");
   tSensorOneLed.setCallback(turnOffSensorOneLed);
 }
 
 void sensorOneHandler() {
-  static unsigned long lastTime = 0;
+  noInterrupts();
+  static bool firstRun = true;
   digitalWrite(SENSOR_ONE_INDICATOR_PIN, HIGH);
 
   // used to measure time between pulses
   unsigned long currentTime = millis();
 
-  if(lastTime == 0) {
-    // This is the first pulse
-    lastTime = currentTime;
-    tSensorOneLed.enable();
+  if(firstRun) {
+    lastPulseTime = currentTime;
+    firstRun = false;
+    return;
+  } else if(currentTime - lastPulseTime <= SENSOR_COOLDOWN) {
+    // the LED on the meter have a tendency to flicker with approx 3-10 ms
+    // between on-off. This makes sure we do not record them.
     return;
   }
-
   pulseCount++;
 
   // calc power
-  power = (3600000000.0 / (currentTime - lastTime)) / ppwh;
+  power = (3600000.0 / (currentTime - lastPulseTime)) / ppwh;
 
   // calc consumed kWh
-  elapsedkWh = (1.0 * pulseCount / (ppwh * 1000)); //multiply by 1000 to convert pulses per wh to kwh
+  todaykWh = (1.0 * pulseCount / (ppwh * 1000)); //multiply by 1000 to convert pulses per wh to kwh
 
-  //Print the values.
-  Serial.print(power,4);
-  Serial.print(" ");
-  Serial.println(elapsedkWh,3);
+  Log("%d\t%.4f W\t%.3f kWh\n", pulseCount, power, todaykWh);
 
-  Log("Power: %.4f W\n", power);
-  Log("Consumed: %.3f kWh\n", elapsedkWh);
+  char tmp[80];
+  sprintf(tmp, "%d\t%.4f W\t%.3f kWh ms: %lu\n", pulseCount, power, todaykWh, millis());
+  publishMQTT(DebugTopic, tmp);
 
-  lastTime = currentTime;
+  lastPulseTime = currentTime;
   tSensorOneLed.enable();
+  interrupts();
+}
+
+void sendTelemetry() {
+  const size_t bufferSize = JSON_OBJECT_SIZE(1) + JSON_OBJECT_SIZE(5);
+  DynamicJsonBuffer respBuffer(bufferSize);
+  JsonObject& json = respBuffer.createObject();
+
+  time_t t = (long) timeClient.getEpochTime();
+
+  char buf[80];
+  sprintf(buf, "%d-%d-%dT%s", year(t), month(t), day(t), timeClient.getFormattedTime().c_str());
+  json["Time"] = buf;
+  
+  JsonObject& energy = json.createNestedObject("ENERGY");
+  energy["Power"] = power;
+  energy["Today"] = todaykWh;
+  energy["Total"] = totalkWh;
+  energy["Yesterday"] = yesterdaykWh;
+
+  String tele = "";
+  json.printTo(tele);
+  publishMQTT(TelemetryTopic, tele);
 }
 
 void setup() {
@@ -335,16 +467,23 @@ void setup() {
 
   EEPROM.begin(512);
   // read old data from eeprom
-  readFromEEPROM();
+  // resetEEPROM();
+  // readFromEEPROM();
 
-  pinMode(SENSOR_ONE_PIN, INPUT);
-  attachInterrupt(digitalPinToInterrupt(SENSOR_ONE_PIN), sensorOneHandler, RISING);
 
   setupWifiManager();
   setupOTA();
+  setupMQTT();
+  timeClient.begin();
 
   tCheckWifiStatus.enable();
-  tSaveToEEPROM.enable();
+  // tSaveToEEPROM.enable();
+  tUpdateTime.enable();
+  tSendTelemetry.enable();
+
+  // lastly enable interrupts
+  pinMode(SENSOR_ONE_PIN, INPUT);
+  attachInterrupt(digitalPinToInterrupt(SENSOR_ONE_PIN), sensorOneHandler, RISING);
   Log("\nEnergy meter setup done\n\n");
 }
 
@@ -355,5 +494,23 @@ void loop() {
     interrupts();
   }
 
+  if(!mqttclient.connected()) {
+    connectMQTT();
+  } {
+    mqttclient.loop();
+  }
+
   taskManager.execute();
+  
+  // see if next day
+  currentDay = timeClient.getDay();
+  if(currentDay != lastDay) {
+    // a new day
+    lastDay = currentDay;
+    yesterdaykWh = todaykWh;
+    totalkWh += todaykWh;
+    todaykWh = 0;
+    pulseCount = 0;
+    saveToEEPROM();
+  } 
 }
